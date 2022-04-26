e72916629c1c97aae1af58a58de62da8410fd9a6
MMijoc <markomijoc18@gmail.com>
2022-04-26 14:53:00 +0200

    Refactored code: Removed unnecessary test because it is contained in another test.

--- BowlingGameKata/BowlingGameKata/GameTest.cs
+++ BowlingGameKata/BowlingGameKata/GameTest.cs
@@ -5,14 +5,6 @@ namespace BowlingGameKata;
 public class GameTest
 {
 	[Fact]
-	public void CanMakeGame()
-	{
-		Game game = new Game();
-
-		Assert.NotNull(game);
-	}
-
-	[Fact]
 	public void CanRollBall()
 	{
 		Game game = new Game();
